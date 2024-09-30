5805f7099873fa66927eb3f5fcccb6868eb4f28d
Agustin Dubal <agustin.dubal06@gmail.com>
2024-09-30 10:24:40 -0300

    feat: agregar tareas para todos los sensores

--- Software/sensado_distancia.c
+++ Software/sensado_distancia.c
@@ -1,22 +1,36 @@
-#include "libs/hysrf05.h"
-#include <wiringPi.h>
 #include <stdio.h>
 #include <pthread.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "hysrf05.h"
+#include <wiringPi.h>
 
-// Configuración de pines del sensor
-#define SENSOR_TRIGGER_PIN 5
-#define SENSOR_ECHO_PIN 18
+// Configuración de pines para múltiples sensores
+#define SENSOR_1_TRIGGER_PIN 5
+#define SENSOR_1_ECHO_PIN 18
 
-// Función de tarea para medir la distancia
+#define SENSOR_2_TRIGGER_PIN 6
+#define SENSOR_2_ECHO_PIN 19
+
+#define SENSOR_3_TRIGGER_PIN 12
+#define SENSOR_3_ECHO_PIN 20
+
+#define SENSOR_4_TRIGGER_PIN 13
+#define SENSOR_4_ECHO_PIN 21
+
+#define SENSOR_5_TRIGGER_PIN 16
+#define SENSOR_5_ECHO_PIN 22
+
+
+// Función de tarea para medir la distancia de un sensor
 void task_measure_distance(void *pvParameters) {
-    HY_SRF05 sensor = {SENSOR_TRIGGER_PIN, SENSOR_ECHO_PIN};
-    HY_SRF05_init(&sensor);
+    HY_SRF05 *sensor = (HY_SRF05 *)pvParameters;
+
+    HY_SRF05_init(sensor);
 
     while (1) {
-        float distance = HY_SRF05_measure_distance(&sensor);
-        printf("Distancia medida: %.2f cm\n", distance);
+        float distance = HY_SRF05_measure_distance(sensor);
+        printf("Distancia medida en el sensor con trigger pin %d: %.2f cm\n", sensor->trigger_pin, distance);
         vTaskDelay(pdMS_TO_TICKS(100));  // Esperar 100 ms antes de la próxima medición
     }
 }
@@ -25,8 +39,19 @@ int main() {
     // Inicializar wiringPi
     wiringPiSetupGpio();
 
-    // Crear la tarea de FreeRTOS
-    xTaskCreate(task_measure_distance, "MeasureDistanceTask", 2048, NULL, 1, NULL);
+    // Crear instancias de los sensores
+    HY_SRF05 sensor1 = {SENSOR_1_TRIGGER_PIN, SENSOR_1_ECHO_PIN};
+    HY_SRF05 sensor2 = {SENSOR_2_TRIGGER_PIN, SENSOR_2_ECHO_PIN};
+    HY_SRF05 sensor3 = {SENSOR_3_TRIGGER_PIN, SENSOR_3_ECHO_PIN};
+    HY_SRF05 sensor4 = {SENSOR_4_TRIGGER_PIN, SENSOR_4_ECHO_PIN};
+    HY_SRF05 sensor5 = {SENSOR_5_TRIGGER_PIN, SENSOR_5_ECHO_PIN};
+
+    // Crear tareas para cada sensor
+    xTaskCreate(task_measure_distance, "MeasureDistanceSensor1", 2048, &sensor1, 1, NULL);
+    xTaskCreate(task_measure_distance, "MeasureDistanceSensor2", 2048, &sensor2, 1, NULL);
+    xTaskCreate(task_measure_distance, "MeasureDistanceSensor3", 2048, &sensor3, 1, NULL);
+    xTaskCreate(task_measure_distance, "MeasureDistanceSensor4", 2048, &sensor4, 1, NULL);
+    xTaskCreate(task_measure_distance, "MeasureDistanceSensor5", 2048, &sensor5, 1, NULL);
 
     // Iniciar el scheduler de FreeRTOS
     vTaskStartScheduler();
